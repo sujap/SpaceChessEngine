04cfa423af6d2f04132bbef7d8573257f5f9912e
Parakram Majumdar <appu226@yahoo.co.in>
2021-01-24 14:47:56 +0000

    Add basic cmake project, and license

---
+++ SpaceChessEngine/SpaceChessEngine.cpp
@@ -0,0 +1,12 @@
+﻿// SpaceChessEngine.cpp : Defines the entry point for the application.
+//
+
+#include "SpaceChessEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

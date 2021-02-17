fd077d8e246e80143b36786b410777d22a50a6c8
Parakram Majumdar <appu226@yahoo.co.in>
2021-02-17 01:05:26 +0000

    Add support for FEN format.

--- chess_test/chess_test.cpp
+++ chess_test/chess_test.cpp
@@ -1,6 +1,14 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+#include <chess/board.h>
+#include <chess/board_impl.h>
+#include <chess/fen.h>
 
-TEST(Suite1, Test1) {
-	EXPECT_EQ(true, true);
-	EXPECT_EQ(true, true);
+
+TEST(BoardSuite, StartingBoardTest) {
+	using namespace space;
+	IBoard::Ptr startingBoard = BoardImpl::getStartingBoard();
+	auto startingFen = Fen::fromBoard(startingBoard, 0, 1);
+	std::string expectedStartingFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+	ASSERT_EQ(startingFen.fen, expectedStartingFen);
+	ASSERT_EQ(Fen::fromBoard(BoardImpl::fromFen(startingFen), 0, 1).fen, expectedStartingFen);
 }
